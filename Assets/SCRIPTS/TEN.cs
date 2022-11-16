8db0587ceb02e3947b66b5ac2bcf019204781be9
MASEPA12 <114388814+MASEPA12@users.noreply.github.com>
2022-11-16 18:33:41 +0100

    list finished

---
+++ Assets/SCRIPTS/TEN.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEN : MonoBehaviour
+{
+    // VARIABLE DELCARAION
+    public int year;
+    private int result1;
+    private int result2;
+    private int result3;
+
+    void Start()
+    {
+        result1 = year % 4; //year/4
+        result2 = year % 100; //year/100
+        result3 = year % 400; //year/400
+        
+        
+        if (result1 == 0) //if it's divisible entre 4
+        {
+            Debug.Log($"The year {year} is a leap year.");
+            if (result2 == 0) //es divisible entre 100
+            {
+                if(result3 == 0) //es divisible entre 400
+                {
+                    Debug.Log($"The year {year} is a leap year.");
+                }
+            }
+        }
+        else
+        {
+            Debug.Log($"The year {year} is not a leap year.");
+        } 
+    }
+}
